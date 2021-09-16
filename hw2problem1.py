148436ac5a8be75320511fa2ad9d7dc38a31fe5b
josephkibira <33089347+97joseph@users.noreply.github.com>
2021-09-16 16:50:16 +0300

    Initial commit

---
+++ hw2problem1.py
@@ -0,0 +1,32 @@
+# PUT YOUR NAME HERE
+# PUT YOUR SBU ID NUMBER HERE
+# PUT YOUR NETID (BLACKBOARD USERNAME) HERE
+#
+# IAE 101 (Fall 2021)
+# HW 2, Problem 1
+
+def divisible(n):
+    # ADD YOUR CODE HERE
+    return -1 # CHANGE OR REMOVE THIS LINE
+
+
+# DO NOT DELETE THE FOLLOWING LINES OF CODE! YOU MAY
+# CHANGE THE FUNCTION CALLS TO TEST YOUR WORK WITH
+# DIFFERENT INPUT VALUES.
+if __name__ == "__main__":
+    test1 = divisible(126)
+    print("divisible(126) is:", test1)
+    print()
+    test2 = divisible(20)
+    print("divisible(20) is:", test2)
+    print()
+    test3 = divisible(1024)
+    print("divisible(1024) is:", test3)
+    print()
+    test4 = divisible(17)
+    print("divisible(17) is:", test4)
+    print()
+    test5 = divisible(539)
+    print("divisible(539) is:", test5)
+    print()
+
---
+++ hw2problem5.py
@@ -0,0 +1,32 @@
+# PUT YOUR NAME HERE
+# PUT YOUR SBU ID NUMBER HERE
+# PUT YOUR NETID (BLACKBOARD USERNAME) HERE
+#
+# IAE 101 (Fall 2021)
+# HW 2, Problem 5
+
+def is_palindrome(s):
+    # ADD YOUR CODE HERE
+    return -1 # CHANGE OR REMOVE THIS LINE
+
+
+# DO NOT DELETE THE FOLLOWING LINES OF CODE! YOU MAY
+# CHANGE THE FUNCTION CALLS TO TEST YOUR WORK WITH
+# DIFFERENT INPUT VALUES.
+if __name__ == "__main__":
+    test1 = is_palindrome("racecar")
+    print("is_palindrome(\"racecar\") is:", test1)
+    print()
+    test2 = is_palindrome("raceboat")
+    print("is_palindrome(\"raceboat\") is:", test2)
+    print()
+    test3 = is_palindrome("a man a plan a canal panama")
+    print("is_palindrome(\"a man a plan a canal panama\") is:", test3)
+    print()
+    test4 = is_palindrome("a place to call up")
+    print("is_palindrome(\"a place to call up\") is:", test4)
+    print()
+    test5 = is_palindrome("deified")
+    print("is_palindrome(\"deified\") is:", test5)
+    print()
+
